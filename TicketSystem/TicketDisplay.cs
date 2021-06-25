404934aca91ce4c48be6a548d30974178794b650
J Loo <junzhongloo@gmail.com>
2021-06-26 11:27:50 +1200

    Update TicketDisplay.cs

--- TicketSystem/TicketDisplay.cs
+++ TicketSystem/TicketDisplay.cs
@@ -29,7 +29,6 @@ namespace TicketSystem
             var tickets = ticketContainer.GetTickets();
 
             Console.WriteLine("Printing Tickets: \n");
-
             foreach (Ticket ticket in tickets)
             {
                 Console.WriteLine($"Ticket Number: {ticket.TicketNumber}");
